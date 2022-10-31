a8d97dab82ad70d3aa20a47241414e9c41e60afe
rn0x <ry2n1996@gmail.com>
2022-10-31 05:32:11 +0300

    Fix: Back to main menu

--- matrix/menu/sendComment/sendComment_2.js
+++ matrix/menu/sendComment/sendComment_2.js
@@ -5,7 +5,6 @@ import sendComment from '../../../discourse/sendComment.js';
 export default {
     async exec({ meId, roomId, sender, name, checkRoom, roomIdOrAlias, body, replyBody, replySender, roomName, event_id, usersAdmin, RichReply, event, client }) {
 
-        let config = fs.readJsonSync(`./config.json`);
         let memberJson = fs.readJsonSync(`./database/matrix/member/${sender}.json`);
 
         if (body) {
@@ -25,10 +24,12 @@ export default {
                 let topic_slug = seCo?.topic_slug
                 let topic_id = seCo?.topic_id
                 let post_number = seCo?.post_number
-                let message = `<b>تم نشر التعليق ✅ <a href='${config?.url}/t/${topic_slug}/${topic_id}'>${post_number}</a></b>`
+                let message = `<b>تم نشر التعليق ✅ <a href='${process.env.url}/t/${topic_slug}/${topic_id}'>${post_number}</a></b>`
                 let reply = RichReply.createFor(roomId, event, message, message);
                 await client.sendMessage(roomId, reply).catch(error => console.log(error));
             }
+
+            await database_matrix_member({ sender: sender, menu: 'main' }).catch(error => console.log(error));
         }
 
         else {
